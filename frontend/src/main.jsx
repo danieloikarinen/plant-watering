e55bb04440978e947edd74303be1a1538fe796d7
Dansku <mirodaniel@proton.me>
2025-11-10 20:57:02 +0200

    add axios import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import axios from "axios";
 import './index.css'
 import App from './App.jsx'
 
